6954a2fcf6e3cccb422de138beb9610dcfad77b3
Omar AbdElkareem <omarkimo80@gmail.com>
2018-12-12 15:19:46 +0200

    Team 7 - count of inputs & outputs

---
+++ Team 7 - Integration with Gauss-Legendre.py
@@ -0,0 +1,9 @@
+''' 
+* no. of inputs: 4 
+	- 3 integers
+		- 2 [limits of integration which may have values from -1e9 (refers to negative infinity) to 1e9 (refers to infinity)] 
+		- 1 [count of points in gauss formula which may have value of 2 or 3 only]
+	- 1 string [the function which will be integrated]
+* no. of outputs: 1 [Value of Integration]
+'''
+
