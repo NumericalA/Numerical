de46e2fa8386ddd2c0bc02302b8954511c4d0d8d
Ahmad Khaled <akregeb@gmail.com>
2018-12-13 22:42:49 +0200

    Create integration.py

---
+++ integration.py
@@ -0,0 +1,7 @@
+'''
+  inputs: (f, a, b, n)
+  f => function to be integrated or table with n values f(x_i) i=1 to n sampled uniformly in [a, b].
+  a => start of integration interval,
+  b => end of integration interval,
+  n => number of sample points
+'''
